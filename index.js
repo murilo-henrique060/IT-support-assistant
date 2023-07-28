1d433983a5e9fba84cb0b163a6dc3c097718e600
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2023-07-28 13:50:21 +0000

    first prototype complete

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Support } = require('./support.js');
 const qrcode = require('qrcode-terminal');
 
+var debug_users = require('./debug-users.json');
+
 const AUTH = !(process.env.AUTH === 'true');
+const DEBUG = (process.env.DEBUG === 'true');
 
 let client = null;
+
+let supports = new Map;
 
 if (AUTH) {
     client = new Client({
@@ -19,20 +25,35 @@ client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', () => {
-    console.log('Client Authenticated!');
-});
-
 client.on('auth_failure', () => {
     console.log('Authentication Failed');
 });
 
-client.on('ready', () => {
-    console.log('Listening...!');
+client.on('ready', async () => {
+    let username = client.info.pushname;
+    let phone = await client.getFormattedNumber(client.info.wid._serialized);
+    console.log('Connected with', phone, 'as', username);
 });
 
-client.on('message', msg => {
-    client.sendMessage(msg.from, 'Olá, eu sou um bot!');
+client.on('message', async msg => {
+    if (DEBUG) {
+        if (!debug_users.includes(msg.from)) {
+            return;
+        }
+    }
+
+    let done = false;
+
+    if (supports.has(msg.from)) {
+        done = await supports.get(msg.from).on_message(msg);
+    } else {
+        supports.set(msg.from, new Support(msg, client));
+        done = await supports.get(msg.from).on_message(msg);
+    }
+
+    if (done) {
+        supports.delete(msg.from);
+    }
 });
 
 client.initialize();
